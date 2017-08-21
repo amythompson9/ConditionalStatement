8b2f7846a2c12479676078d76487974c422a90b8
amythompson9 <amythompson9@gmail.com>
2017-08-21 13:46:53 -0400

    first commit

---
+++ ConditionalStatement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 21;
+            if (age >= 17)
+            {
+                Console.WriteLine("You can see this movie");
+
+            }
+            else
+            {
+                Console.WriteLine("Sorry you are too young.  You need a parent present.");
+            }
+    }
+}
