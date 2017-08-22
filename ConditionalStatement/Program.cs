139cab297100bb9903e72b546298db792f4b0376
amythompson9 <amythompson9@gmail.com>
2017-08-21 20:50:21 -0400

    More in class conditionals

--- ConditionalStatement/Program.cs
+++ ConditionalStatement/Program.cs
@@ -77,52 +77,62 @@ namespace ConditionalStatement
             //{
             //    Console.WriteLine("You have earned a D.");
             //}
-            
 
 
-            Console.WriteLine("Please tell me your age");
-            int studentAge = int.Parse(Console.ReadLine());
-            if (studentAge < 0.0)
-            {
-                Console.WriteLine("This program is for humans.");
-            }
-            if (studentAge > 100.0)
-            {
-                Console.WriteLine("This program is for humans.");
-            }
-            else if (studentAge >= 0 && studentAge <= 2)
-            {
-                Console.WriteLine("Still in Mama's Arms.");
-            }
-            else if (studentAge >= 3 && studentAge <= 4)
-            {
-                Console.WriteLine("Pre-School Maniac.");
-            }
-            else if (studentAge >= 5 && studentAge <= 11)
-            {
-                Console.WriteLine("Elementary School.");
-            }
-            else if (studentAge >= 12 && studentAge <= 14)
-            {
-                Console.WriteLine("Middle School.");
-            }
-            else if (studentAge >= 15 && studentAge <= 18)
-            {
-                Console.WriteLine("High School.");
-            }
-            else if (studentAge >= 19 && studentAge <= 22)
-            {
-                Console.WriteLine("College.");
-            }
-            else if (studentAge >= 23 && studentAge <= 65)
-            {
-                Console.WriteLine("Working for the Man.");
-            }
-            else if (studentAge >= 66 && studentAge <= 100)
-            {
-                Console.WriteLine("Golden Years.");
-            }
 
+            //Console.WriteLine("Please tell me your age");
+            //int studentAge = int.Parse(Console.ReadLine());
+            //if (studentAge < 0.0)
+            //{
+            //    Console.WriteLine("This program is for humans.");
+            //}
+            //if (studentAge > 100.0)
+            //{
+            //    Console.WriteLine("This program is for humans.");
+            //}
+            //else if (studentAge >= 0 && studentAge <= 2)
+            //{
+            //    Console.WriteLine("Still in Mama's Arms.");
+            //}
+            //else if (studentAge >= 3 && studentAge <= 4)
+            //{
+            //    Console.WriteLine("Pre-School Maniac.");
+            //}
+            //else if (studentAge >= 5 && studentAge <= 11)
+            //{
+            //    Console.WriteLine("Elementary School.");
+            //}
+            //else if (studentAge >= 12 && studentAge <= 14)
+            //{
+            //    Console.WriteLine("Middle School.");
+            //}
+            //else if (studentAge >= 15 && studentAge <= 18)
+            //{
+            //    Console.WriteLine("High School.");
+            //}
+            //else if (studentAge >= 19 && studentAge <= 22)
+            //{
+            //    Console.WriteLine("College.");
+            //}
+            //else if (studentAge >= 23 && studentAge <= 65)
+            //{
+            //    Console.WriteLine("Working for the Man.");
+            //}
+            //else if (studentAge >= 66 && studentAge <= 100)
+            //{
+            //    Console.WriteLine("Golden Years.");
+            //}
+
+            Console.WriteLine("Please enter a number.");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number + " is an even number.");
+            }
+            else
+            {
+                Console.WriteLine(number + " is an odd number.");
+            }
         }
     }
 }
